9e1e2365a283545426a73a7d5d55c084c9be888d
Mayuresh Naik <Mayuresh.Naik@Remote.local>
2023-10-04 18:37:36 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/layout/Navbar";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import ContactState from "./context/contact/ContactState";
+import Register from "./components/auth/Register";
+import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
+import Alerts from "./components/layout/Alerts";
+import setAuthToken from "./utils/setAuthToken";
+import Login from "./components/auth/Login";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
 }
+
+const App = () => {
+  return (
+    <AuthState>
+    <ContactState>
+      <AlertState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Alerts/>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+              <Route exact path="/register" element={<Register />} />
+              <Route exact path="/login" element={<Login />} />
+            </Routes>
+          </div>
+        </Fragment>
+      </Router>
+      </AlertState>
+    </ContactState>
+    </AuthState>
+  );
+};
 
 export default App;
---
+++ src/components/routing/PrivateRoute.js
@@ -0,0 +1,15 @@
+// import React, {useContext} from 'react'
+// import AuthContext from '../../context/auth/authContext'
+
+// const PrivateRoute = ({component: Component, ...rest}) => {
+//     const authContext = useContext(AuthContext);
+//     const {isAuthenticated, loading} = authContext;
+//   return (
+//     <Route {...rest} render={props => !isAuthenticated && !loading ?(
+//         <Redirect to= '/login'/>
+
+//     })/>
+//   )
+// }
+
+// export default PrivateRoute
